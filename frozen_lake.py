60f9a0b4338bdc84bb19cbac26bbc5e65a1db5c1
eshouppe <eshouppe@gmail.com>
2017-05-13 23:19:57 -0400

    simple q-network learning implemented

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,13 +1,15 @@
 """
 Solve OpenAI gym Frozen Lake test problem trying different algorithms.
 """
+import random
 import gym
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
 def frzn_lk_q_tbl_lrn():
-    """Q-learning as frozen lake problem solving approach"""
+    """Q-learning tables as frozen lake problem solving approach"""
     # Load the environment
     env = gym.make('FrozenLake-v0')
 
@@ -46,25 +48,86 @@ def frzn_lk_q_tbl_lrn():
 
 #
 def frzn_lk_q_ntwrk_lrn():
-    pass
+    """Q-learning networks as frozen lake problem solving approach"""
+    # Load the environment
+    env = gym.make('FrozenLake-v0')
+
+    tf.reset_default_graph()
+    # Feed forward part of network. Used to select action.
+    inputsl = tf.placeholder(shape=[1, 16], dtype=tf.float32)
+    W = tf.Variable(tf.random_uniform(shape=[16, 4], minval=0, maxval=0.01))
+    Qout = tf.matmul(inputsl, W)
+    predict = tf.argmax(Qout, 1)
+
+    # Sum-of-squares loss between target and predicted Q-values
+    nextQ = tf.placeholder(shape=[1, 4], dtype=tf.float32)
+    ss_loss = tf.reduce_sum(tf.square(nextQ - Qout))
+    trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+    update_model = trainer.minimize(ss_loss)
+
+    init = tf.initialize_all_variables()
+    # Learning parameters
+    y = 0.99
+    e = 0.1
+    num_episodes = 2000
+    # Lists are total rewards and steps per episode
+    jList = []
+    rList = []
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(num_episodes):
+            # Reset environment. Get new observation.
+            s = env.reset()
+            rAll = 0
+            d = False
+            j = 0
+            while j < 99:
+                j += 1
+                # Greedily choose action with e chance of random action
+                a, allQ = sess.run([predict, Qout], feed_dict={inputsl: np.identity(16)[s:s+1]})
+                if np.random.rand(1) < e:
+                    a[0] = env.action_space.sample()
+                # Get new state and reward from environment
+                sl, r, d, _ = env.step(a[0])
+                # Obtain Q' values by feeding new state thru network
+                Ql = sess.run(Qout, feed_dict={inputsl: np.identity(16)[sl:sl+1]})
+                # Obtain maximium Q' and set target value
+                maxQl = np.max(Ql)
+                targetQ = allQ
+                targetQ[0, a[0]] = r + y * maxQl
+                # Train the network
+                _, Wl = sess.run([update_model, W], feed_dict={inputsl: np.identity(16)[s:s+1], nextQ: targetQ})
+                rAll += r
+                s = sl
+                if d:
+                    # Reduce chance of random action of model trains
+                    e = 1.0 / ((i/50) + 10)
+                    break
+            jList.append(j)
+            rList.append(rAll)
+    print("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
+
 
 #
 def plot_reward_by_try():
     """Plot the average reward by try"""
-    plt.plot(range(1, 11), all_avg_rewards)
-    plt.ylim(0.15, 0.85)
+    plt.plot(range(len(all_avg_rewards)), all_avg_rewards)
+    plt.ylim(0, 1)
     plt.xlabel("Try number")
     plt.ylabel("Average reward")
     plt.show()
 
 
 # Execute multiple tries of algorithm and display results
-all_avg_rewards = []
-for new_try in range(10):
-    list_reward, episode_cnt = frzn_lk_q_tbl_lrn()
-    average_reward = sum(list_reward) / float(episode_cnt)
-    print("Average reward: {}".format(average_reward))
-    all_avg_rewards.append(average_reward)
+# all_avg_rewards = []
+# for new_try in range(10):
+#     list_reward, episode_cnt = frzn_lk_q_tbl_lrn()
+#     average_reward = sum(list_reward) / float(episode_cnt)
+#     print("Average reward: {}".format(average_reward))
+#     all_avg_rewards.append(average_reward)
 
-print("Best average reward in 10 tries: {}".format(np.max(all_avg_rewards)))
-plot_reward_by_try()
+# print("Best average reward in 10 tries: {}".format(np.max(all_avg_rewards)))
+# plot_reward_by_try()
+
+frzn_lk_q_ntwrk_lrn()
--- readme.md
+++ readme.md
@@ -1,15 +1,22 @@
-#### Frozen Lake Problem
+### Frozen Lake Problem
 Frozen lake environment in the OpenAI gym has a 4x4 grid of blocks. Each is either a start block, goal block, safe frozen block, or a dangerous hole. 
 
 Agent's objective is to navigate from start to goal block without falling into a hole. A wind occasionally blows the agent onto a different block.
 
 The agent can move up, down, left, or right. No reward for each step. Reward for entering the goal block is 1.
 
-#### Reinforcement Q-Learning
-Reinforcement learning with Policy Networks attempts to learn functions that directly map an observation to an action. In contrast, Q-Learning attempts to learn the value of being in a given state and then takes an action. 
+### Reinforcement Q-Learning
+Reinforcement learning with Policy Networks attempts to learn functions that directly map an observation to an action. In contrast, Q-Learning attempts to learn the value of being in a given state and then takes an action.
 
+#### Q-table learning
 Simple Q-learning implementation for this problem is a table of size number of states by number of allowed actions, which is 16x4 in this game. At the start, the table of Q-values has uniform zeros, but those are updated using the Bellman equation.
 
 Bellman equation: the expected long term reward for a given action equals the immediate reward for the current action combined with the expected reward from the best future action taken at the following state.
 
 Q(state, action) = thisReward + discounted(max(Q(nextState, nextAction)))
+
+#### Q-network learning
+Q-table learning doesn't scale as the number of possible states grows. In lieu of table,
+a network may learn to approximate a function that maps possible states to Q-values.
+
+Simple network implementation encodes the states in a one hot vector and produces a vector of the 4 actions' Q-values. Basically this is the old table in network clothes until fancy stuff like activation functions are added. Also instead of directly updating a table, the network is updated with back propagation. The loss function is the sum-of-squares loss.
